61bdc4bd9f557dc2fd55cc574efc4801e54cc601
Nichita Ghenzeli <nichitag@nycstudents.net>
2023-09-26 09:10:09 -0400

    Initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("This took way too long to set up...");
+        System.out.println("This took way too long to set up..!!" );
 
     }
 }
